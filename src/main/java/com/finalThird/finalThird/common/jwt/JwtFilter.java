c7e8bb382123f261d2625bc6c6b32beab60cc74c
ksw <45636035+troysw@users.noreply.github.com>
2023-10-20 19:01:16 +0900

    [Feat] refresh token 추가

--- src/main/java/com/finalThird/finalThird/common/jwt/JwtFilter.java
+++ src/main/java/com/finalThird/finalThird/common/jwt/JwtFilter.java
@@ -1,19 +1,30 @@
 package com.finalThird.finalThird.common.jwt;
 
+import com.finalThird.finalThird.common.exception.ApiException;
+import com.finalThird.finalThird.common.exception.AuthException;
+import com.finalThird.finalThird.common.exception.ErrorCode;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.json.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class JwtFilter extends GenericFilterBean {
@@ -23,16 +34,25 @@ public class JwtFilter extends GenericFilterBean {
 
   private final TokenProvider tokenProvider;
 
+
   // 실제 필터릴 로직
   // 토큰의 인증정보를 SecurityContext에 저장하는 역할 수행
   @Override
   public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
-    HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-    String jwt = resolveToken(httpServletRequest);
-    String requestURI = httpServletRequest.getRequestURI();
+    HttpServletRequest request = (HttpServletRequest) servletRequest;
+    HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-    if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
-      Authentication authentication = tokenProvider.getAuthentication(jwt);
+
+    String accessToken = resolveAccessToken(request);
+    String refreshToken = resolveRefreshToken(request);
+
+    tokenProvider.tokenAuthentication(response, accessToken, refreshToken);
+
+
+    String requestURI = request.getRequestURI();
+
+    if (StringUtils.hasText(accessToken) && tokenProvider.validateToken(accessToken)) {
+      Authentication authentication = tokenProvider.getAuthentication(accessToken);
       SecurityContextHolder.getContext().setAuthentication(authentication);
       logger.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
     } else {
@@ -43,13 +63,42 @@ public class JwtFilter extends GenericFilterBean {
   }
 
   // Request Header 에서 토큰 정보를 꺼내오기 위한 메소드
-  private String resolveToken(HttpServletRequest request) {
-    String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
-
-    if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-      return bearerToken.substring(7);
+  public String resolveAccessToken(HttpServletRequest request) {
+    String token = null;
+    String accessTokenHeader = request.getHeader(AUTHORIZATION_HEADER);
+    if (accessTokenHeader != null && accessTokenHeader.startsWith("Bearer ")) {
+      token = accessTokenHeader.substring(7);
     }
-
-    return null;
+    return token;
   }
+
+  //추후 소켓 이용시
+//  public String resolveAccessToken(StompHeaderAccessor accessor) {
+//    String token = null;
+//    String accessTokenHeader = accessor.getFirstNativeHeader(AUTHORIZATION_HEADER);
+//    if (accessTokenHeader != null && accessTokenHeader.startsWith("Bearer ")) {
+//      token = accessTokenHeader.substring(7);
+//    }
+//    return token;
+//  }
+
+  public String resolveRefreshToken(HttpServletRequest request) {
+    String token = null;
+    String accessTokenHeader = request.getHeader("refreshToken");
+    if (accessTokenHeader != null && accessTokenHeader.startsWith("Bearer ")) {
+      token = accessTokenHeader.substring(7);
+    }
+    return token;
+  }
+
+  //추후 소켓 이용시
+//  public String resolveRefreshToken(StompHeaderAccessor accessor) {
+//    String token = null;
+//    String accessTokenHeader = accessor.getFirstNativeHeader("refresh-token");
+//    if (accessTokenHeader != null && accessTokenHeader.startsWith("Bearer ")) {
+//      token = accessTokenHeader.substring(7);
+//    }
+//    return token;
+//  }
+
 }
--- src/main/java/com/finalThird/finalThird/common/jwt/TokenProvider.java
+++ src/main/java/com/finalThird/finalThird/common/jwt/TokenProvider.java
@@ -1,41 +1,63 @@
 package com.finalThird.finalThird.common.jwt;
 
 
+import com.finalThird.finalThird.auth.controller.dto.Token;
+import com.finalThird.finalThird.common.config.database.RedisRepository;
+import com.finalThird.finalThird.common.exception.ApiException;
+import com.finalThird.finalThird.common.exception.AuthException;
+import com.finalThird.finalThird.common.exception.ErrorCode;
+import com.finalThird.finalThird.customer.domain.Customer;
+import com.finalThird.finalThird.customer.external.CustomerRepository;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
+import java.io.UnsupportedEncodingException;
 import java.security.Key;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Date;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class TokenProvider implements InitializingBean {
+
+  private final RedisTemplate<String, String> redisTemplate;
 
   private final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
   private static final String AUTHORITIES_KEY = "auth";
-  private final String secret;
-  private final long tokenValidityInMilliseconds;
-  private Key key;
 
-  public TokenProvider(
-      @Value("${jwt.secret}") String secret,
-      @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSeconds) {
-    this.secret = secret;
-    this.tokenValidityInMilliseconds = tokenValidityInSeconds * 1000;
-  }
+  @Value("${jwt.response.header}")
+  private String accessHeader;
+
+  @Value("${jwt.secret}")
+  private String secret;
+  private final long accessTokenTime = (1000L * 60 * 30); // 30분;
+  private final long refreshTokenTime = (1000L * 60 * 60 * 24 * 7); // 1주;
+  private Key key;
+  private final RedisRepository redisRepository;
+  private final CustomerRepository customerReader;
+
 
   // 빈이 생성되고 주입을 받은 후에 secret값을 Base64 Decode해서 key 변수에 할당하기 위해
   @Override
@@ -44,22 +66,84 @@ public class TokenProvider implements InitializingBean {
     this.key = Keys.hmacShaKeyFor(keyBytes);
   }
 
-  public String createToken(Authentication authentication) {
+  public String createAccessToken(Authentication authentication) {
     String authorities = authentication.getAuthorities().stream()
         .map(GrantedAuthority::getAuthority)
         .collect(Collectors.joining(","));
 
     // 토큰의 expire 시간을 설정
     long now = (new Date()).getTime();
-    Date validity = new Date(now + this.tokenValidityInMilliseconds);
+    Date accessTime = new Date(now + this.accessTokenTime);
 
-    return Jwts.builder()
+    String accessToken = Jwts.builder()
         .setSubject(authentication.getName())
         .claim(AUTHORITIES_KEY, authorities) // 정보 저장
         .signWith(key, SignatureAlgorithm.HS512) // 사용할 암호화 알고리즘과 , signature 에 들어갈 secret값 세팅
-        .setExpiration(validity) // set Expire Time 해당 옵션 안넣으면 expire안함
+        .setExpiration(accessTime) // set Expire Time 해당 옵션 안넣으면 expire안함
+        .compact();
+
+
+
+    return accessToken;
+  }
+
+  public String createAccessToken(String userPk, HashMap<String, Object> claim) {
+    return createToken(userPk, claim, accessTokenTime, secret);
+  }
+
+  public String createRefreshToken(String userPk) {
+    return createToken(userPk, null, refreshTokenTime, secret);
+  }
+
+
+  private String createToken(String userPk, HashMap<String, Object> claim, Long expiration, String secret) {
+    Claims claims = Jwts.claims().setSubject(userPk); // JWT payload 에 저장되는 정보단위
+    if (claim != null) {
+      claims.put("userInfo", claim);
+    }
+    Date now = new Date();
+    return Jwts.builder()
+        .setHeaderParam("typ", "JWT")
+        .setClaims(claims) // 정보 저장
+        .setIssuedAt(now) // 토큰 발행 시간 정보
+        .setExpiration(new Date(now.getTime() + expiration)) // set Expire Time
+        .signWith(SignatureAlgorithm.HS256, secret)  // 사용할 암호화 알고리즘과
+        // signature 에 들어갈 secret값 세팅
         .compact();
   }
+
+
+  public String createRefreshToken(Authentication authentication) {
+    String authorities = authentication.getAuthorities().stream()
+        .map(GrantedAuthority::getAuthority)
+        .collect(Collectors.joining(","));
+
+    // 토큰의 expire 시간을 설정
+    long now = (new Date()).getTime();
+    Date refreshTime = new Date(now + this.refreshTokenTime);
+
+    String refreshToken = Jwts.builder()
+        .setSubject(authentication.getName())
+        .claim(AUTHORITIES_KEY, authorities) // 정보 저장
+        .signWith(key, SignatureAlgorithm.HS512) // 사용할 암호화 알고리즘과 , signature 에 들어갈 secret값 세팅
+        .setExpiration(refreshTime) // set Expire Time 해당 옵션 안넣으면 expire안함
+        .compact();
+
+    redisTemplate.opsForValue().set(
+        authentication.getName(),
+        refreshToken,
+        this.refreshTokenTime,
+        TimeUnit.MILLISECONDS
+    );
+    return refreshToken;
+  }
+
+
+
+
+
+
+
 
   // 토큰으로 클레임을 만들고 이를 이용해 유저 객체를 만들어서 최종적으로 authentication 객체를 리턴
   public Authentication getAuthentication(String token) {
@@ -79,6 +163,7 @@ public class TokenProvider implements InitializingBean {
 
     return new UsernamePasswordAuthenticationToken(principal, token, authorities);
   }
+
 
   // 토큰의 유효성 검증을 수행
   public boolean validateToken(String token) {
@@ -100,4 +185,120 @@ public class TokenProvider implements InitializingBean {
     }
     return false;
   }
+
+  public void tokenAuthentication(HttpServletResponse response, String accessToken, String refreshToken) {
+    if (accessToken == null && refreshToken == null) {
+      return;
+    }
+
+    if (refreshToken == null) {
+      accessTokenHandling(accessToken);
+    } else {
+      refreshTokenHandlingResponse(response, accessToken, refreshToken);
+    }
+  }
+
+  private void accessTokenHandling(String accessToken) {
+    if (!validateToken(accessToken)) {
+      throw new AuthException(ErrorCode.INVALID_ACCESS_TOKEN);
+    }
+
+    if (SecurityContextHolder.getContext().getAuthentication() == null) {
+      Authentication authentication = getAccessAuthentication(accessToken);
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+  }
+  public Authentication getAccessAuthentication(String token) {
+    Claims claims = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+    String id = claims.getSubject();
+    Customer user = customerReader.findByCustomerEmail(id).orElseThrow(() -> new ApiException(ErrorCode.USER_NOT_FOUND));
+
+    Collection<? extends GrantedAuthority> authorities;
+
+      authorities = Arrays.stream(user.getAuthorities().toString().split(","))
+          .map(SimpleGrantedAuthority::new)
+          .collect(Collectors.toList());
+
+    return new UsernamePasswordAuthenticationToken(user, token, authorities);
+  }
+
+  private void refreshTokenHandlingResponse(HttpServletResponse response, String accessToken, String refreshToken) {
+
+    String accessJws = refreshTokenHandling(accessToken, refreshToken);
+
+    if (response == null) {
+      return;
+    }
+
+    String header = response.getHeader(accessHeader);
+    if (header == null || "".equals(header)) {
+      response.addHeader(accessHeader, accessJws);
+    }
+  }
+
+  private String refreshTokenHandling(String accessToken, String refreshToken) {
+    if (!validateToken(refreshToken)) {
+      throw new AuthException(ErrorCode.EXPIRED_REFRESH_TOKEN);
+    }
+
+    if (accessToken == null) {
+      throw new AuthException(ErrorCode.INVALID_ACCESS_TOKEN);
+    }
+
+    if (!validateToken(accessToken)) {
+      throw new AuthException(ErrorCode.EXPIRED_REFRESH_TOKEN);
+    }
+
+    String accessTokenDecode = decode(accessToken);
+    org.json.simple.parser.JSONParser parser = new JSONParser();
+    JSONObject object;
+    try {
+      object = (JSONObject) parser.parse(accessTokenDecode);
+    } catch (ParseException e) {
+      log.error("accessToken parsing error", e);
+      throw new AuthException(ErrorCode.RUNTIME_EXCEPTION_TOKEN);
+    }
+
+    String id = object.get("customerEmail").toString();
+    Optional<String> refreshTokenInDB = selectRefreshToken(id);
+
+    if (refreshTokenInDB.isEmpty() || !refreshTokenInDB.get().equals(refreshToken)) {
+      throw new AuthException(ErrorCode.INVALID_REFRESH_TOKEN);
+    }
+
+    Customer user = customerReader.findByCustomerEmail(id).orElseThrow(() -> new ApiException(ErrorCode.USER_NOT_FOUND));
+
+    HashMap<String, Object> claim = new HashMap<>();
+
+    claim.put("customerEmail", user.getCustomerEmail());
+    claim.put("role", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(",")));
+
+    String accessJws = createAccessToken(id, claim);
+    Authentication authentication = getAccessAuthentication(accessJws);
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+
+    return accessJws;
+  }
+
+  public String decode(String token) {
+    String[] splitToken = token.split("\\.");
+    Base64.Decoder decoder = Base64.getDecoder();
+    byte[] decodedBytes = decoder.decode(splitToken[1]);
+
+    String decodedString = null;
+    try {
+      decodedString = new String(decodedBytes, "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
+    }
+    return decodedString;
+  }
+
+  public Optional<String> selectRefreshToken(String userId) {
+    return Optional.ofNullable(redisRepository.getValue(redisRepository.getValue(userId)));
+  }
+
+
+
 }
